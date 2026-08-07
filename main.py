bcbb37821c107826faebc6886a608525bb297ff3
Arthur <arthurscbrito@gmail.com>
2026-08-07 19:17:26 -0300

    correção da semântica 'prs' para 'merges'

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import requests                                                    
-import os                                                           
+import requests                                             
+import os                                                   
 from collections import Counter                                     
 from dotenv import load_dotenv
 
@@ -27,12 +27,10 @@ def merged_prs(owner, repo, token):
     dados = []
     page = 1
     
-    # Laço infinito controlado que varre as páginas
     while True:
         url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
         headers = {"Authorization": f"Bearer {token}"}
         
-        # Otimiza a busca pedindo 100 de vez e controlando a página atual
         params = {"state": "closed", "per_page": 100, "page": page}
 
         response = requests.get(url, headers=headers, params=params)    
@@ -54,36 +52,26 @@ def merged_prs(owner, repo, token):
     return dados
 
 
-# --- REGRA DE NEGÓCIOS: Extração e Filtro de Contribuidores ---
-
 def build_ranking(prs):
-    # 1. Definimos a lista estritamente com letras minúsculas
     mantenedores = ["fxarth", "morcineck", "ohlm1"]
 
-    # 2. Extrai os nomes, usando o .lower() para forçar tudo para minúsculo antes de comparar
     contributors = [
         pr["user"]["login"] 
         for pr in prs 
         if pr["user"]["login"].lower() not in mantenedores
     ]
     
-    # 3. Conta e monta o ranking apenas com a comunidade
     return Counter(contributors).most_common()
 
 
-# ROTA DA API: Aqui é onde o usuário bate quando acessa a web
 @app.get("/ranking")
 def ranking_endpoint():
-    # 1. Pega os PRs usando as variáveis globais
     prs_mergeados = merged_prs(OWNER, REPO, TOKEN)
-    
-    # 2. Gera o ranking (agora sem os mantenedores)
     ranking = build_ranking(prs_mergeados)
     
-    # 3. Retorna um JSON formatado e bonito para o usuário
     return {
         "repositorio": f"{OWNER}/{REPO}",
-        "total_prs_aprovados": len(prs_mergeados),
+        "total_merges": len(prs_mergeados), # <-- Alteração semântica correta
         "quantidade_contribuidores": len(ranking),
         "ranking_top_contribuidores": ranking
     }
